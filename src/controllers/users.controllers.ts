837b2b27c923ada4a187362e56669a8fcbadb44a
MINHTQ3-VD\MinhTQ3 <minhtq3@rikkeisoft.com>
2024-02-29 18:01:58 +0700

    82

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,8 +1,22 @@
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
+import usersService from "~/services/users.services";
 export const loginController = (req: Request, res: Response) => {
-  const { email, password } = req.body;
-
   res.status(200).json({
-    message: "Logic success!"
+    message: "Logic success!",
   });
-}
+};
+
+export const registerController = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  try {
+    const result = await usersService.register({ email, password });
+    res.status(200).json({
+      message: "Register success!",
+      result,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Register failed!",
+    });
+  }
+};
