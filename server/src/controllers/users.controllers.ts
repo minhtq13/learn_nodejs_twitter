ec273d1336129c66cc2e61d1101ca67651c47227
MINHTQ3-VD\MinhTQ3 <minhtq3@rikkeisoft.com>
2024-03-12 17:11:25 +0700

    116.1

--- server/src/controllers/users.controllers.ts
+++ server/src/controllers/users.controllers.ts
@@ -38,6 +38,7 @@ export const oauthController = async (req: Request, res: Response) => {
   const result = await usersService.oauth(code as string);
   const urlRedirect = `${process.env.CLIENT_REDIRECT_CALLBACK as string}?access_token=${result.access_token}&refresh_token=${result.refresh_token}&new_user=${result.newUser}verify=${result.verify}`;
   return res.redirect(urlRedirect);
+  
 };
 
 export const registerController = async (
